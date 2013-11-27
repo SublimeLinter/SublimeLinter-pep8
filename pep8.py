1e19eb4f58b523b229461745c2d875ade76bd130
Aparajita Fishman <aparajita@aparajita.com>
2013-11-27 01:01:43 -0500

    Implemented checking directly in plugin if possible.

--- pep8.py
+++ pep8.py
@@ -10,12 +10,42 @@
 
 """This module exports the PEP8 plugin linter class."""
 
+import os
+
+try:
+    from pep8 import StandardReport
+except ImportError:
+    StandardReport = None
+
 from SublimeLinter.lint import PythonLinter
+
+
+if StandardReport is not None:
+
+    class Report(StandardReport):
+
+        """Provides a report in the form of a single multiline string, without printing."""
+
+        def get_file_results(self):
+            """Collect and return the results for this file."""
+            self._deferred_print.sort()
+            results = ''
+
+            for line_number, offset, code, text, doc in self._deferred_print:
+                results += '{path}:{row}:{col}: {code} {text}\n'.format_map({
+                    'path': self.filename,
+                    'row': self.line_offset + line_number,
+                    'col': offset + 1,
+                    'code': code,
+                    'text': text
+                })
+
+            return results
 
 
 class PEP8(PythonLinter):
 
-    """Provides an interface to the pep8 python script."""
+    """Provides an interface to the pep8 python module/script."""
 
     language = 'python'
     cmd = ('pep8@python', '*', '-')
@@ -28,3 +58,24 @@ class PEP8(PythonLinter):
     }
     inline_settings = 'max-line-length'
     inline_overrides = ('select', 'ignore')
+    module = 'pep8'
+
+    def check(self, code, filename):
+        """Run pep8.Checker on code and return the output."""
+
+        view_settings = self.get_view_settings()
+
+        kwargs = {
+            'select': view_settings.get('select', ''),
+            'ignore': view_settings.get('ignore', ''),
+            'max_line_length': view_settings.get('max-line-length'),
+            'reporter': Report
+        }
+
+        checker = self.module.Checker(
+            filename=os.path.basename(filename),
+            lines=code.splitlines(keepends=True),
+            **kwargs
+        )
+
+        return checker.check_all()
